f04546264661528e161e99b696fbf81d00516319
Swapnil Bhade <swapnil.bhade@radinik.com>
2021-04-07 16:30:51 +0530

    Add Authentication in API

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,21 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services , IConfiguration context){
+            services.AddScoped<ITokenService , TokenService>();
+            services.AddDbContext<DataContext>(options => {
+                options.UseSqlite(context.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+    }
+}
---
+++ API/Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,25 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtension
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services , IConfiguration context){
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(context["TokenKey"])),
+                            ValidateIssuer = false,
+                            ValidateAudience = false,
+                        };
+                    });
+                    return services;
+        }
+    }
+}
